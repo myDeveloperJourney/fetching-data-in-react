7fd6f78fcd24b3742aa9b9cb0c8e6ac8283c12af
Daniel Scott <daniel@mydeveloperjourney.com>
2025-03-27 15:32:03 -0500

    wip: creates weather service module

---
+++ src/services/weatherService.js
@@ -0,0 +1,15 @@
+const API_KEY = '';
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+        const queryString = `&q=${city}`;
+        const res = await fetch(BASE_URL + queryString);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export { show }; // "named" export syntax
